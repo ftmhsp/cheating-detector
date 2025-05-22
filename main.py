7bc16c42dbf5aa1506aee5917ec924b9ce7f69aa
ftm.hsp <fatemeh84haleh@gmail.com>
2025-05-22 14:30:39 +0330

    FastAPI main app with API routes for submission and cheating analysis - written by fatemeh

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from schemas import StudentResponse
+from database import save_student_response, fetch_all_responses
+from cheat_analysis_transformer import analyze_similarities, analyze_time
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/submit")
+def submit_response(student: StudentResponse):
+    save_student_response(student)
+    return {"message": "Student response saved successfully", "student_id": student.student_id}
+
+@app.get("/analyze")
+def analyze_cheating():
+    student_data = fetch_all_responses()
+    print("🔍 داده‌های واکشی‌شده:", student_data)
+    text_matches = analyze_similarities(student_data, similarity_threshold=0.5)
+    time_matches = analyze_time(student_data, time_threshold=15)
+
+    return {
+        "text_similarity_suspects": text_matches,
+        "time_similarity_suspects": time_matches
+    }
